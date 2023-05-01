9e4156b26f2efea9bc79da30c0b22202823ce46c
Shambhavi Mallikarjuna <shambz@Shambhavis-MacBook-Pro.local>
2023-05-01 16:39:54 +0530

    New fronty

--- soch-dump-takeone/src/land-components/Register.js
+++ soch-dump-takeone/src/land-components/Register.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from "react"
 import {Link} from "react-router-dom"
-import PhoneInput, {isValidPhoneNumber} from "react-phone-number-input";
+// import PhoneInput, {isValidPhoneNumber} from "react-phone-number-input";
 import './Register.css';
 
 
